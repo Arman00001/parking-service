75d1e3b1115ac6668533cd3de594d9399d84d452
Arman <melkonyan.arman.03@gmail.com>
2025-08-02 01:47:23 +0400

    Custom Community dto for returning data from the table

---
+++ src/main/java/com/arman/parkingservice/dto/community/CommunityResponse.java
@@ -0,0 +1,12 @@
+package com.arman.parkingservice.dto.community;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommunityResponse {
+    private Long id;
+
+    private String name;
+}
