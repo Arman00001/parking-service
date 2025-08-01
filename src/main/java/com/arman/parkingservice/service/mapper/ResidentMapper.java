e883750c06390cb64f817d9e264d0f55d7a51e10
Arman <melkonyan.arman.03@gmail.com>
2025-08-02 01:03:58 +0400

    Resident mapper to dto and from dto

---
+++ src/main/java/com/arman/parkingservice/service/mapper/ResidentMapper.java
@@ -0,0 +1,31 @@
+package com.arman.parkingservice.service.mapper;
+
+import com.arman.parkingservice.dto.community.CommunityResponse;
+import com.arman.parkingservice.dto.resident.ResidentCreateDto;
+import com.arman.parkingservice.dto.resident.ResidentResponse;
+import com.arman.parkingservice.persistence.entity.Community;
+import com.arman.parkingservice.persistence.entity.Resident;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResidentMapper {
+    public Resident mapCreateToResident(ResidentCreateDto residentCreateDto, Community community) {
+        Resident resident = new Resident();
+        resident.setFirstName(residentCreateDto.getFirstName());
+        resident.setLastName(residentCreateDto.getLastName());
+        resident.setEmail(residentCreateDto.getEmail());
+        resident.setCommunity(community);
+
+        return resident;
+    }
+
+    public ResidentResponse mapToResponse(Resident resident, CommunityResponse communityResponse) {
+        ResidentResponse residentResponse = new ResidentResponse();
+        residentResponse.setId(resident.getId());
+        residentResponse.setFirstName(resident.getFirstName());
+        residentResponse.setLastName(residentResponse.getLastName());
+        residentResponse.setCommunity(communityResponse);
+
+        return residentResponse;
+    }
+}
