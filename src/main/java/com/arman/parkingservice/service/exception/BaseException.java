4805c472a67f2f5049badc274ef0bbf1e628099a
Arman <melkonyan.arman.03@gmail.com>
2025-08-02 01:00:00 +0400

    Custom base exception

---
+++ src/main/java/com/arman/parkingservice/service/exception/BaseException.java
@@ -0,0 +1,7 @@
+package com.arman.parkingservice.service.exception;
+
+public class BaseException extends RuntimeException {
+    public BaseException(String message) {
+        super(message);
+    }
+}
