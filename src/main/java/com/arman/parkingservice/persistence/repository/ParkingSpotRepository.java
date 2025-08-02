1c33154af912417b3ded2925070a19aa171f6eb7
Arman <melkonyan.arman.03@gmail.com>
2025-08-02 17:00:30 +0400

    Api to fetch available parking spots in a given time frame within a community, with criteria

---
+++ src/main/java/com/arman/parkingservice/persistence/repository/ParkingSpotRepository.java
@@ -0,0 +1,44 @@
+package com.arman.parkingservice.persistence.repository;
+
+import com.arman.parkingservice.criteria.ParkingSpotSearchCriteria;
+import com.arman.parkingservice.dto.parkingspot.ParkingSpotResponse;
+import com.arman.parkingservice.persistence.entity.Community;
+import com.arman.parkingservice.persistence.entity.ParkingSpot;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, Long> {
+    Optional<ParkingSpot> findByCommunityAndCode(Community community, String code);
+
+    @Query("""
+        SELECT new com.arman.parkingservice.dto.parkingspot.ParkingSpotResponse(
+            p.id,
+            p.code,
+            p.community,
+            p.status
+        )
+        FROM ParkingSpot p
+        WHERE p.community.id = :communityId
+            AND :#{#criteria.code} IS NULL OR LOWER(p.code) LIKE LOWER(CONCAT('%',:#{#criteria.code},'%'))
+            AND p.id NOT IN (
+                SELECT b.parkingSpot.id
+                FROM Booking b
+                WHERE b.bookingStatus IN ('RESERVED','ACTIVE')
+                    AND b.startTime <: endTime
+                    AND b.endTime >: startTime
+        )
+""")
+    Page<ParkingSpotResponse> findAllAvailableByCommunityAndCriteria(
+            Long communityId,
+            LocalDateTime startTime,
+            LocalDateTime endTime,
+            ParkingSpotSearchCriteria criteria,
+            Pageable pageable);
+}
