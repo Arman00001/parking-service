64bc28bfe0f21beaf6f40407c767ec6b7e022b19
Arman <melkonyan.arman.03@gmail.com>
2025-08-02 16:37:47 +0400

    Make all arguments constructor for ResidentResponse

--- src/main/java/com/arman/parkingservice/dto/resident/ResidentResponse.java
+++ src/main/java/com/arman/parkingservice/dto/resident/ResidentResponse.java
@@ -1,11 +1,14 @@
 package com.arman.parkingservice.dto.resident;
 
 import com.arman.parkingservice.dto.community.CommunityResponse;
+import com.arman.parkingservice.persistence.entity.Community;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class ResidentResponse {
     private Long id;
 
@@ -14,4 +17,11 @@ public class ResidentResponse {
     private String lastName;
 
     private CommunityResponse community;
+
+    public ResidentResponse(Long id, String firstName, String lastName, Community community) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.community = new CommunityResponse(community.getId(), community.getName());
+    }
 }
