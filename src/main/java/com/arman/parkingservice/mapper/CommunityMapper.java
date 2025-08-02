940f62310dc15fd5a68ba609ca7eb9116e1c4dda
Arman <melkonyan.arman.03@gmail.com>
2025-08-02 16:51:05 +0400

    Add CommunityCreateDto to Community mapper

--- src/main/java/com/arman/parkingservice/mapper/CommunityMapper.java
+++ src/main/java/com/arman/parkingservice/mapper/CommunityMapper.java
@@ -1,5 +1,6 @@
 package com.arman.parkingservice.mapper;
 
+import com.arman.parkingservice.dto.community.CommunityCreateDto;
 import com.arman.parkingservice.dto.community.CommunityResponse;
 import com.arman.parkingservice.persistence.entity.Community;
 import org.springframework.stereotype.Component;
@@ -12,5 +13,12 @@ public class CommunityMapper {
         communityResponse.setName(community.getName());
 
         return communityResponse;
+    }
+
+    public Community mapCreateToCommunity(CommunityCreateDto communityCreateDto) {
+        Community community = new Community();
+        community.setName(communityCreateDto.getName());
+
+        return community;
     }
 }
