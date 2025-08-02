74f3b13aad5f11fedaec574cb5670d825dc89842
Arman <melkonyan.arman.03@gmail.com>
2025-08-02 20:30:59 +0400

    Create Booking entity/table

---
+++ src/main/java/com/arman/parkingservice/persistence/entity/Booking.java
@@ -0,0 +1,42 @@
+package com.arman.parkingservice.persistence.entity;
+
+import com.arman.parkingservice.enums.BookingStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "booking")
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "resident_id", nullable = false)
+    private Resident resident;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "spot_id", nullable = false)
+    private ParkingSpot parkingSpot;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endTime;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "booking_status", nullable = false)
+    private BookingStatus bookingStatus;
+
+    @Column(name = "actual_start_date", nullable = false)
+    private LocalDateTime actualStartTime;
+
+    @Column(name = "actual_end_date", nullable = false)
+    private LocalDateTime actualEndTime;
+}
