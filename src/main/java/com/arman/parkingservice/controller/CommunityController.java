0c4de2f8dac98e56b668aef2c540c16c695536ee
Arman <melkonyan.arman.03@gmail.com>
2025-08-02 16:48:19 +0400

    Api for adding and fetching Community

--- src/main/java/com/arman/parkingservice/controller/CommunityController.java
+++ src/main/java/com/arman/parkingservice/controller/CommunityController.java
@@ -10,11 +10,14 @@ import com.arman.parkingservice.dto.parkingspot.ParkingSpotResponse;
 import com.arman.parkingservice.dto.resident.ResidentResponse;
 import com.arman.parkingservice.service.CommunityService;
 import com.arman.parkingservice.service.ResidentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/communities")
